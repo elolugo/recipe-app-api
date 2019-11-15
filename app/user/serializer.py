2bd67850dd57eb0ab4dd58967d5dec06ac8d5bb1
Alejandro Lugo <elo.lugo@gmail.com>
2019-11-15 18:20:18 -0300

    Hotfix flake8 issues

--- app/user/serializer.py
+++ app/user/serializer.py
@@ -62,6 +62,7 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+
 class AuthTokenSerializer(serializers.Serializer):
     """ Serializer for the user authentication object"""
 
