0c23757ad5dbdf093f0a9369f224bf683349a0c1
Alejandro Lugo <elo.lugo@gmail.com>
2019-11-20 12:03:18 -0300

    Re-factored tests and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,18 +7,10 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """
-    Manage Tags in the database
-    """
-
-    """
-    What permissions the user must have to access this API.
-    Setting that the user must be authenticated first to acccess the API
-    """
-    permission_classes = (IsAuthenticated,)
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """ Base Viewset for user owned recipe attributes """
 
     """
     How the authentication will be made eg: Cookie, session, token.
@@ -26,49 +18,10 @@ class TagViewSet(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication,)
 
     """
-    The queryset that should be used for returning objects from this view.
-    When using GET, what would be the queryset to return as a response.
-    """
-    queryset = Tag.objects.all()
-
-    serializer_class = serializers.TagSerializer
-
-    def get_queryset(self):
-        """ Return objects for the current authenticated user only """
-
-        """
-        Filtering the queryset before responding the GET request
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """ Runs everytime a new POST request is made """
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
-    """ Manage ingredients in the database """
-
-    """
     What permissions the user must have to access this API.
     Setting that the user must be authenticated first to acccess the API
     """
     permission_classes = (IsAuthenticated,)
-
-    """
-    How the authentication will be made eg: Cookie, session, token.
-    """
-    authentication_classes = (TokenAuthentication,)
-
-    """
-    The queryset that should be used for returning objects from this view.
-    When using GET, what would be the queryset to return as a response.
-    """
-    queryset = Ingredient.objects.all()  # Attribute of GenericAPIView
-
-    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         """
@@ -84,3 +37,29 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """ What to do if there is a POST request """
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """
+    Manage Tags in the database
+    """
+
+    """
+    The queryset that should be used for returning objects from this view.
+    When using GET, what would be the queryset to return as a response.
+    """
+    queryset = Tag.objects.all()  # Attribute of GenericAPIView
+
+    serializer_class = serializers.TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """ Manage ingredients in the database """
+
+    """
+    The queryset that should be used for returning objects from this view.
+    When using GET, what would be the queryset to return as a response.
+    """
+    queryset = Ingredient.objects.all()  # Attribute of GenericAPIView
+
+    serializer_class = serializers.IngredientSerializer
